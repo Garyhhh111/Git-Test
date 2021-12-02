8f688e4150706345d121df495b88b44591bbf5e2
Gary <921899945@qq.com>
2021-12-03 02:37:56 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/Git/GitTest.java
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello2 world");
         System.out.println("hello3 world");
+        System.out.println("合并");
     }
 }
