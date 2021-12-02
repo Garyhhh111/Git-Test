7d1e735c6815fc8ecbf66c70dacbf6fa9baae396
Gary <921899945@qq.com>
2021-12-03 01:49:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.Git;
+
+/**
+ * @author Gary
+ * @create 2021-12-03 1:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
